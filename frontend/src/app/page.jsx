91fb90d3874eb3f75b7fdce1a68e00b2a41814f5
aviral1702 <aviralchandra18@gmail.com>
2024-02-28 19:37:12 +0530

    Home Page

--- frontend/src/app/page.jsx
+++ frontend/src/app/page.jsx
@@ -1,32 +1,39 @@
 'use client';
 import React from 'react';
-import * as css from './page.module.css';
 import { useRouter } from 'next/navigation';
-// import GraphQL from './assets/GraphQL.mp4';
+import backgroundImage from '../../public/blackbg.png';
+import './HomePage.css';
+import ScrollReveal from 'scrollreveal';
 
 const Home = () => {
   const Router = useRouter();
   return (
-    <div className="container">
-      <div className='row ms-5 me-5 mt-5'>
-        <p className="fs-2">
-          If you've seen a GraphQL query before, you know that the GraphQL query language is basically about selecting fields on objects. So, for example, in the following query:
-        </p>
-      </div>
-      <div className="row ms-5 me-5">
-        <div className='table-responsive'>
-          <table className='table table-bordered border-primary'  >
-            <thead>
-              <tr>
-                <th className='fs-5 text-center'>Query</th>
-                <th className='fs-5 text-center'>Result</th>
-              </tr>
-            </thead>
-            <tbody>
-            <tr>
-              <td>
-                <pre className='fs-6'>
-                  {`
+    <div className="container-fluid">
+      {/* <video muted autoPlay loop id='bg-video'>
+        <source src="BgVideo.mp4" type="video/mp4" /></video> */}
+      <div style={{
+        backgroundImage: `url("https://nordicapis.com/wp-content/uploads/Walkthrough-of-Using-GraphQL-Shield-1024x576.png")`,
+        backgroundSize: "cover"
+      }}>
+        <div className='row px-5'>
+          <p className="fs-2 pt-5 text-black" id='intro'>
+            If you've seen a GraphQL query before, you know that the GraphQL query language is basically about selecting fields on objects. So, for example, in the following query:
+          </p>
+        </div>
+        <div className="row px-5 pb-5">
+          <div className='table-responsive d-flex justify-content-center'>
+            <table className='table table-bordered table-dark table-hover table-sm w-50' id='table1'>
+              <thead>
+                <tr>
+                  <th className='fs-5 text-center'>Query</th>
+                  <th className='fs-5 text-center'>Result</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <td>
+                    <pre className='fs-6'>
+                      {`
 {
   hero {
     name
@@ -34,11 +41,11 @@ const Home = () => {
   }
 }                  
                 `}
-                </pre>
-              </td>
-              <td>
-                <pre className='fs-6'>
-                  {`
+                    </pre>
+                  </td>
+                  <td>
+                    <pre className='fs-6'>
+                      {`
 {
     "data": {
       "hero": {
@@ -52,189 +59,164 @@ const Home = () => {
     }
   }
   `}
-                </pre>
-              </td>
-            </tr>
-            </tbody>
-          </table>
+                    </pre>
+                  </td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
         </div>
       </div>
-      <div className="row ms-5 me-5 mt-4">
-        <div className='col-md-3'>
-          <video className="w-100 h-100" muted autoPlay>
-            <source src="/GraphQL.mp4" type="video/mp4" />
-          </video>
-        </div>
-        <div className="col-md-3">
-          <h4>Describe your data</h4>
-          <p>Describe your data in the schema definition language (SDL)</p>
-          <div className='table-responsive'>
-            <table className='table table-borderless table-dark'  >
-              <tbody>
-              <tr>
-                <td>
-                  <pre>
-                    {`
+
+      <div style={{
+        backgroundImage: `url("https://w0.peakpx.com/wallpaper/410/412/HD-wallpaper-plain-black-black.jpg")`,
+        backgroundSize: "contain"
+      }}>
+        <div className="row px-5 p-5">
+          <div className='col-md-3'>
+            <video className="w-100 h-100" muted autoPlay loop>
+              <source src="/GraphQL.mp4" type="video/mp4" />
+            </video>
+          </div>
+          <div className="col-md-3 text-white">
+            <h4>Describe your data</h4>
+            <p>Describe your data in the schema definition language (SDL)</p>
+            <div className='table-responsive'>
+              <table className='table table-borderless table-sm' id='table2'  >
+                <tr>
+                  <td>
+                    <pre className='text-white'>
+                      {`
 type Project {
   name: String
   tagline: String
   contributors: [User]
 }
 `}
-                  </pre>
-                </td>
-              </tr>
-              </tbody>
-            </table>
+                    </pre>
+                  </td>
+                </tr>
+              </table>
+            </div>
           </div>
-        </div>
 
-        <div className="col-md-3">
-          <h4>Ask for what you want</h4>
-          <p>Specify the operations you want to perform.</p>
-          <div className='table-responsive'>
-            <table className='table table-borderless table-dark'  >
-              <tr>
-                <td>
-                  <pre>
-                    {`
+          <div className="col-md-3 text-white">
+            <h4>Ask for what you want</h4>
+            <p>Specify the operations you want to perform on your data.</p>
+            <div className='table-responsive'>
+              <table className='table table-borderless table-sm' id='table2'  >
+                <tr>
+                  <td>
+                    <pre className='text-white'>
+                      {`
 {
   project(name: "GraphQL") {
     tagline
   }
 }
 `}
-                  </pre>
-                </td>
-              </tr>
-            </table>
+                    </pre>
+                  </td>
+                </tr>
+              </table>
+            </div>
           </div>
-        </div>
 
-        <div className="col-md-3">
-          <h4>Get desired response</h4>
-          <p>Get the exact data you asked for...............</p>
-          <div className='table-responsive'>
-            <table className='table table-borderless table-dark'  >
-              <tr>
-                <td>
-                  <pre>
-                    {`
+          <div className="col-md-3 text-white">
+            <h4>Get desired response</h4>
+            <p>Get the exact data you asked for, nothing more, nothing less.</p>
+            <div className='table-responsive'>
+              <table className='table table-borderless table-sm' id='table2'  >
+                <tr>
+                  <td>
+                    <pre className='text-white'>{`
 {
   "project": {
     "tagline": "A query language"
   }
 }
 `}
-                  </pre>
-                </td>
-              </tr>
-            </table>
+                    </pre>
+                  </td>
+                </tr>
+              </table>
+            </div>
           </div>
         </div>
+        <div className="d-flex justify-content-center pb-5">
+          <button onClick={() => Router.push('login')} type="button" className="btn btn-outline-light p-2 m-2 w-25 text-white">Login</button>
+          <button onClick={() => Router.push('signup')} type="button" className="btn btn-outline-light p-2 m-2 w-25 text-white">Signup</button>
+        </div>
       </div>
-      <div className="d-flex justify-content-center m-5">
-        <button onClick={() => Router.push('login')} type="button" id='button' className="btn btn-outline-dark p-2 m-2">Login</button>
-        <button onClick={() => Router.push('signup')} type="button" id='button' className="btn btn-outline-dark p-2 m-2">Signup</button>
+      <div className='d-flex justify-content-center p-5'>
+        <h4 className="text-center px-5 py-4 w-50">GraphQL is a query language for your API, and a runtime for executing those queries by using a type system you define for your data. GraphQL isn't tied to any specific database or storage engine and is instead backed by your existing code and data.</h4>
       </div>
-    </div>
+      <div className='d-flex justify-content-center'>
+        <h4 className="text-center px-5 pb-5 w-50">A GraphQL service is created by defining types and fields on those types, then providing functions for each field on each type.</h4>
+      </div>
+      <div style={{
+        backgroundImage: `url("https://w0.peakpx.com/wallpaper/410/412/HD-wallpaper-plain-black-black.jpg")`,
+        backgroundSize: "contain"
+      }}>
+        <div className='d-flex justify-content-center'>
+          <h2 className='text-center px-5 py-5' id='code_generate'>Want auto generated Queries or the backend code for your application ?</h2>
+          <h3 className='text-center px-5 py-5' id='code_generate'>This tool helps you to build queries by leveraging your API's type system.</h3>
+        </div>
+        <div className='d-flex justify-content-center text-white'>
+          <h3 className='text-center' id='code_generate'>Click on the below button to get the code.</h3>
+        </div>
+        <div className="d-flex justify-content-center p-5">
+          <button onClick={() => Router.push('/login')} type="button" className="btn btn-outline-light text-white p-2 m-2 w-25">Get Started</button>
+        </div>
+      </div>
+      <div className='d-flex justify-content-center'>
+        <h1 className='text-center px-5 py-4'>How GraphQL is different from REST ?</h1>
+      </div>
+      <div className='table-responsive d-flex justify-content-center'>
+        <table className='table table-bordered table-hover table-striped w-75'>
+          <thead className='text-center'>
+            <tr>
+              <th>Parameter</th>
+              <th>GraphQL</th>
+              <th>REST</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>What is it?</td>
+              <td>GraphQL is a query language, architecture style, and set of tools to create and manipulate APIs.</td>
+              <td>REST is a set of rules that defines structured data exchange between a client and a server.</td>
+            </tr>
+            <tr>
+              <td>Data access</td>
+              <td>GraphQL has a single URL endpoint.</td>
+              <td>REST has multiple endpoints in the form of URLs to define resources.</td>
+            </tr>
+            <tr>
+              <td>Data returned</td>
+              <td>GraphQL returns data in a flexible structure defined by the client.</td>
+              <td>REST returns data in a fixed structure defined by the server.</td>
+            </tr>
+            <tr>
+              <td>How data is structured and defined</td>
+              <td>GraphQL data is strongly typed. So the client receives data in predetermined and mutually understood formats.</td>
+              <td>REST data is weakly typed. So the client must decide how to interpret the formatted data when it is returned.</td>
+            </tr>
+            <tr>
+              <td>Error checking</td>
+              <td>With GraphQL, invalid requests are typically rejected by schema structure. This results in an autogenerated error message.</td>
+              <td>With REST, the client must check if the returned data is valid.</td>
+            </tr>
+            <tr>
+              <td> </td>
+              <td><img src="GraphQLDiagram.png" alt="" width={400} height={400} /></td>
+              <td><img src="RestDiagram.png" alt="" width={400} height={400} /></td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </div >
   )
 }
 
 export default Home
-
-// import Image from "next/image";
-// import styles from "./page.module.css";
-
-// export default function Home() {
-//   return (
-//     <main className={styles.main}>
-//       <div className={styles.description}>
-//         <p>
-//           Get started by editing&nbsp;
-//           <code className={styles.code}>src/app/page.js</code>
-//         </p>
-//         <div>
-//           <a
-//             href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-//             target="_blank"
-//             rel="noopener noreferrer"
-//           >
-//             By{" "}
-//             <Image
-//               src="/vercel.svg"
-//               alt="Vercel Logo"
-//               className={styles.vercelLogo}
-//               width={100}
-//               height={24}
-//               priority
-//             />
-//           </a>
-//         </div>
-//       </div>
-
-//       <div className={styles.center}>
-//         <Image
-//           className={styles.logo}
-//           src="/next.svg"
-//           alt="Next.js Logo"
-//           width={180}
-//           height={37}
-//           priority
-//         />
-//       </div>
-
-//       <div className={styles.grid}>
-//         <a
-//           href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-//           className={styles.card}
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           <h2>
-//             Docs <span>-&gt;</span>
-//           </h2>
-//           <p>Find in-depth information about Next.js features and API.</p>
-//         </a>
-
-//         <a
-//           href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-//           className={styles.card}
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           <h2>
-//             Learn <span>-&gt;</span>
-//           </h2>
-//           <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-//         </a>
-
-//         <a
-//           href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-//           className={styles.card}
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           <h2>
-//             Templates <span>-&gt;</span>
-//           </h2>
-//           <p>Explore starter templates for Next.js.</p>
-//         </a>
-
-//         <a
-//           href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-//           className={styles.card}
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           <h2>
-//             Deploy <span>-&gt;</span>
-//           </h2>
-//           <p>
-//             Instantly deploy your Next.js site to a shareable URL with Vercel.
-//           </p>
-//         </a>
-//       </div>
-//     </main>
-//   );
-// }
